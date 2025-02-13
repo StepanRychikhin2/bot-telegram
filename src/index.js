74fb0fcc5fec5b60c6074b8d507a72fe824e5936
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-02-13 15:05:27 +0200

    awdad

--- src/index.js
+++ src/index.js
@@ -60,18 +60,23 @@ function handleNoClick() {
 	noButton.textContent = messages[messageIndex]
 	messageIndex = (messageIndex + 1) % messages.length
 	const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize)
+	const currenth = parseFloat(window.getComputedStyle(yesButton).height)
+	const currentw = parseFloat(window.getComputedStyle(yesButton).width)
+	console.log(currentw)
 	yesButton.style.fontSize = `${currentSize * 1.5}px`
+	yesButton.style.height = `${currenth * 1.5}px`
+	yesButton.style.width = `${currentw * 1.5}px`
 	if (messageIndex === 5) {
-        yesButton.style.zIndex = 100
+        // yesButton.style.zIndex = 100
         noButton.style.position = 'static'
 		console.log(123)
 		// yesButton.style.position = 'absolute'
 		// yesButton.style.top = 0
 		// yesButton.style.left = 0
-		// yesButton.style.width = '100%'
-		// yesButton.style.height = '100%'
+		yesButton.style.width = '100%'
+		yesButton.style.height = '100%'
 		// yesButton.style.fontSize = `80px`
-        yesButton.classList = "last"
+        yesButton.classList.add("last") 
 		// top: 0;
 		// left: 0;
 		// position: absolute;
